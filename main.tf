e58f04526cbddfc5e5302fa291fb62a6da062ac6
Praveen K. Gostu <praveek9@in.ibm.com>
2021-04-30 11:22:45 +0530

    Update test

--- main.tf
+++ main.tf
@@ -1,29 +1,16 @@
-resource "ibm_container_cluster" "kubecluster" {
-  name              = "${var.cluster_name}${random_id.name.hex}"
-  datacenter        = "${var.datacenter}"
-  default_pool_size = "${var.default_pool_size}"
-  machine_type      = "${var.machine_type}"
-  hardware          = "${var.hardware}"
-  kube_version      = "${var.kube_version}"
-  public_vlan_id    = "${var.public_vlan_id}"
-  private_vlan_id   = "${var.private_vlan_id}"
-  lifecycle {
-    ignore_changes = ["kube_version"]
-  }
-}
 
-resource "random_id" "name" {
-  byte_length = 4
-}
 
 provider "helm" {
   kubernetes {
-    config_path = "${data.ibm_container_cluster_config.cluster_config.config_file_path}"
+    config_path = "${data.ibm_container_cluster_config.cluster_foo.config_file_path}"
   }
 }
 
+data "ibm_container_cluster_config" "cluster_foo" {
+  cluster_name_id = "${var.cluster_name_id}"
+}
+
 resource "helm_chart" "myredis" {
-   depends_on = ["ibm_container_cluster.kubecluster"]
    name      = "my_redis"
    chart     = "stable/redis"
 }
--- variables.tf
+++ variables.tf
@@ -1,28 +1,7 @@
-variable "ibmcloud_api_key" {}
+
 
 variable "region" {
   default = "us-east"
 }
-variable "machine_type" {}
-# variable "isolation" {}
-variable "private_vlan_id" {}
-variable "public_vlan_id" {}
-# variable "subnet_id" {}
 
-variable "hardware" {}
-
-variable "datacenter" {
-  default = "wdc07"
-}
-
-variable "default_pool_size" {
-  default = "3"
-}
-
-variable "cluster_name" {
-  default = "cluster"
-}
-variable kube_version {
-  #default = "3.11.104_openshift"
-  default = "1.13.9"
-}
+variable "cluster_name_id" {}
